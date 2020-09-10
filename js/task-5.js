d3c0cc2268b912af4c17a16bc5f11a6dc196a330
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2020-09-11 00:43:35 +0300

    5 task

--- js/task-5.js
+++ js/task-5.js
@@ -3,77 +3,174 @@
 
 class Car {
   /*
-   * Добавь статический метод `getSpecs(car)`,
-   * который принимает объект-машину как параметр и выводит
-   * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+   * Добавь `статический` метод
+   * `getSpecs(car)`, который принимает
+   * объект-машину как параметр
+   * и возвращает шаблонную строку
+   * со свойствами и значениями объекта.
+   * Свойства:
+   *   maxSpeed,
+   *   speed,
+   *   isOn,
+   *   distance,
+   *   price
+   * Пример строки, полученной этим методом:
+   * 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
    */
 
 /*
  * Конструктор получает объект настроек.
  *
  * Добавь свойства будущеего экземпляра класса:
- *  speed - текущая скорость, изначально 0
+ *  speed - текущая скорость,
+ *          начальное значение `0`
  *  price - цена автомобиля
  *  maxSpeed - максимальная скорость
- *  isOn - заведен ли автомобиль, значения true или false. Изначально false
- *  distance - общий киллометраж, изначально 0
+ *  isOn - заведен ли автомобиль.
+ *         Значения `true` или `false`,
+ *         начальное значение false
+ *  distance - пробег в километрах,
+ *             начальное значение `0`
  */
-constructor() { }
+//constructor() { }
 
 /*
- * Добавь геттер и сеттер для свойства price,
- * который будет работать с свойством цены автомобиля.
+ * Добавь геттер и сеттер
+ * для свойства `price`, который будет
+ * работать с свойством цены автомобиля.
+ *
+ * ВАЖНО: для записи методов get и set
+ * значение параметра записывают с
+ * подчеркиванием. См. ниже пример 1.
  */
 
 /*
- * Добавь код для того чтобы завести автомобиль
- * Записывает в свойство isOn значение true
+ * Метод, который заводит автомобиль
+ * Записывает в свойство `isOn` значение `true`
  */
-turnOn() { }
+//turnOn() { }
 
 /*
- * Добавь код для того чтобы заглушить автомобиль
- * Записывает в свойство isOn значение false,
- * и сбрасывает текущую скорость в 0
+ * Метод, чтобы заглушить автомобиль
+ * Этот метод должен записывать
+ * в свойство isOn значение false,
+ * и сбрасывать текущую скорость до 0
  */
-turnOff() { }
+//turnOff() { }
 
 /*
- * Добавялет к свойству speed полученное значение,
- * при условии что результирующая скорость
- * не больше чем значение свойства maxSpeed
+ * Этот метод должен добавлять
+ * к свойству `speed` полученное
+ * значение, при условии,
+ * что результирующая скорость
+ * не больше чем значение свойства `maxSpeed`
  */
-accelerate(value) { }
+//accelerate(value) { }
 
 /*
- * Отнимает от свойства speed полученное значение,
- * при условии что результирующая скорость не меньше нуля
+ * Этот метод должен отнимать
+ * от свойства `speed`
+ * полученное значение, при условии,
+ * что результирующая скорость не меньше 0
  */
-decelerate(value) { }
+
+//decelerate(value) { }
 
 /*
- * Добавляет в поле distance киллометраж (hours * speed),
- * но только в том случае если машина заведена!
+ * Этот метод должен добавлять к свойству
+ * `distance` пробег в километрах,
+ * т.е. hours * speed,
+ * но только в том случае,
+ * если машина заведена!
  */
-drive(hours) { }
+//drive(hours) { }
+//}
+
+//#### Пример 1.
+
+/* ```js
+class Guest {
+  // Собственные свойства класса размещаем в конструкторе
+  constructor(name, roomNumber) {
+    /* параметр name не должен 
+     *   совпадать с названием свойства
+     *   но должен быть похож.
+     *   Поэтому используют прием смены
+     *   названия добавляя символ '_'.
+     *   Хотя это может любой другой символ
+     */
+/*this._name = name;
+  }
+
+  // Используем геттеры и сеттеры для описания интерфейса доступа к свойствам
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+}
+ */
+
+class Car {
+  // Write code under this line
+  static getSpecs(car) {
+    return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`;
+  }
+  constructor({ maxSpeed, speed = 0, isOn = false, distance = 0, price } = {}) {
+    this.speed = speed;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    this.isOn = true;
+    //console.log(this.isOn);
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    if (this.speed + value <= this.maxSpeed) {
+      this.speed += value;
+    } else this.speed = this.maxSpeed;
+  }
+  decelerate(value) {
+    if (this.speed - value >= 0) {
+      this.speed -= value;
+    } else this.speed = 0;
+  }
+  drive(hours) {
+    if (this.isOn === true) {
+      this.distance += hours * this.speed;
+    }
+  }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
-
 mustang.turnOn();
 mustang.accelerate(50);
 mustang.drive(2);
 
-Car.getSpecs(mustang);
-// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
 
 mustang.decelerate(20);
 mustang.drive(1);
 mustang.turnOff();
 
-Car.getSpecs(mustang);
-// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
 
 console.log(mustang.price); // 2000
 mustang.price = 4000;
-console.log(mustang.price); // 4000 */
+console.log(mustang.price); // 4000
